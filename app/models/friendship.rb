bb6bba0c4f2c1ea5a800478ff2bcca8febe769a2
India Meisner <india.rae@gmail.com>
2013-10-13 21:27:14 -0700

    Create Friendship model

---
+++ app/models/friendship.rb
@@ -0,0 +1,6 @@
+class Friendship < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :friend, :class_name => "User"
+  attr_accessible :friends, :invited
+
+end
--- db/migrate/20131014035727_create_users.rb
+++ db/migrate/20131014035727_create_users.rb
@@ -4,8 +4,7 @@ class CreateUsers < ActiveRecord::Migration
       t.string :first_name
       t.string :last_name
       t.string :email
-      t.string :password
-      t.string :confirm_password
+      t.string :password_digest
 
       t.timestamps
     end
