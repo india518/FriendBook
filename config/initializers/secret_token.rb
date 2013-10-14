ad9b1960dd370cc4b158bbbd3d22227fd45365ea
India Meisner <india.rae@gmail.com>
2013-10-13 20:46:11 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FriendBook::Application.config.secret_token = '263ba173d1b90447b629bb509e6d89d808d75a179b259d61d543cb14cee83972921ced50470991e9a21fb7e2177256d2bca4245fd44b0bc310c7075e685cff06'
