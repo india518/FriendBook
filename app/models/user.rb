906336f2c556b591ab50894e9edf0db45eba4f95
India Meisner <india.rae@gmail.com>
2013-10-13 21:19:46 -0700

    Create User model

---
+++ app/models/user.rb
@@ -0,0 +1,32 @@
+class User < ActiveRecord::Base
+	CRAZY_EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]+)\z/i
+
+  attr_accessible :email, :first_name, :last_name, :password, :confirm_password
+  has_many :friendships
+  has_many :friends, :through => :friendships, :class_name => "User", :foreign_key => "friend_id"
+
+  validates :first_name,	:last_name,	:presence		=> true,
+																			:length			=> { :within => 1..50 }
+	validates :email,				:presence	=> true,
+													:format 	=> { :with 		=> CRAZY_EMAIL_REGEX,
+																			 :message	=> "The email provided is not a valid email address."},
+													:uniqueness	=> { :case_sensitive => false }
+	validates :password,								:presence		=> true,
+																			:length			=> { :within => 6..40 }
+
+	before_save { self.email = email.downcase } 
+	has_secure_password
+
+	def name
+		"#{self.first_name} #{self.last_name}"
+	end
+
+	def invites_sent
+		#query to get list of people you have invited
+	end
+
+	def invites_recieved
+		#query to get list of people you have recieved invites from
+	end
+
+end
