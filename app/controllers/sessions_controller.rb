9e68d412e57cf17e3ad3fc865404922c2eab6cc1
India Meisner <india.rae@gmail.com>
2013-10-25 14:00:38 -0700

    Updated login(sessions#create) controller to give a better error message (indicating if the email or the password caused the login to fail). Did this as an example for a student question, decided to keep it.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,15 @@ class SessionsController < ApplicationController
 	end
 
 	def create
-		user = User.find_by_email(params[:session][:email]).try(:authenticate, params[:session][:password])
+		user = User.find_by_email(params[:session][:email])
 		if user == false || user.nil?
 			redirect_to login_url,
-									:flash => { :alert => 'Incorrect email and/or password.' }
+			:flash => { :alert => 'No user exists with that email' }
+
+		user.try(:authenticate, params[:session][:password])
+		if user == false || user.nil?
+			redirect_to login_url,
+			:flash => { :alert => 'Incorrect email and/or password.' }
 		else
 			sign_in user
 			redirect_to user_url(user)
